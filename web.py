2396cffe9f7f879d38a4a0177fa540d9cdce8d65
Juliette Lonij <juliette.lonij@kb.nl>
2016-02-02 17:05:49 +0100

    Fix bug

--- web.py
+++ web.py
@@ -1,22 +1,28 @@
 import os, sys
-os.chdir(os.path.dirname(__file__))
+#os.chdir(os.path.dirname(__file__))
 sys.path.insert(0, os.path.dirname(__file__))
 
 
 from bottle import abort, route, run, template, request, default_app
-from disambiguation import Linker
+import disambiguation
 import hashlib
 
 
 @route('/link')
 def link():
+    os.chdir(os.path.dirname(__file__))
+
+    #fh=open('/tmp/tmppploggg', 'a')
+    #fh.write(os.getcwd()  + "\n")
+    #fh.close()
+
     if request.params.get('ne') is not None:
         debug = request.params.get('debug')
         ne = request.params.get('ne')
         ne_type = request.params.get('ne_type')
 	url = request.params.get('url')
 
-        linker = Linker()
+        linker = disambiguation.Linker()
 	if url and ne_type:
             link, p, mainLabel, reason = linker.link(ne, ne_type, url)
 	else:
