92f3d0bb4e285f11ff379f5527a3d5c917c23046
Juliette Lonij <jlonij@gmail.com>
2016-06-13 21:55:30 +0200

    Improve dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,4 @@
-nnnn = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag',
+days = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag',
 'zondag']
 
 months = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli',
@@ -10,177 +10,218 @@ genders = {
     }
 
 types = {
-    'person': {
-        'schema_types': ['Person', 'Agent']
-    },
-    'location': {
-        'schema_types': ['Place', 'Location']
-    },
-    'organisation':
-        'schema_types': ['Organization', 'Organisation']
+    'person': ['Person', 'Agent'],
+    'location': ['Place', 'Location'],
+    'organisation': ['Organization', 'Organisation']
     }
 
 roles = {
     # Persons
     'politician': {
-        'types': ['person'],
-        'schema_types': ['Politician', 'OfficeHolder', 'Judge',
-            'MemberOfParliament', 'President', 'PrimeMinister',
-            'Governor', 'Congresman'],
         'words': ['minister', 'premier', 'kamerlid', 'partijleider',
             'burgemeester', 'staatssecretaris', 'president',
             'wethouder', 'consul', 'ambassadeur', 'gemeenteraadslid',
-            'fractieleider', 'politicus']
+            'fractieleider', 'politicus'],
+        'schema_types': ['Politician', 'OfficeHolder', 'Judge',
+            'MemberOfParliament', 'President', 'PrimeMinister',
+            'Governor', 'Congressman', 'Mayor'],
+        'subjects': ['politics'],
+        'types': ['person']
         },
     'royalty': {
-        'types': ['person'],
-        'schema_types': ['Royalty', 'Monarch', 'Noble'],
         'words': ['keizer', 'koning', 'koningin', 'vorst', 'prins',
-            'prinses', 'kroonprins', 'kroonprinses']
+            'prinses', 'kroonprins', 'kroonprinses', 'majesteit'],
+        'schema_types': ['Royalty', 'Monarch', 'Noble'],
+        'subjects': ['politics'],
+        'types': ['person']
         },
     'military_person': {
-        'types': ['person'],
-        'schema_types': ['MilitaryPerson'],
         'words': ['generaal', 'gen', 'majoor', 'maj', 'luitenant',
-            'kolonel', 'kol', 'kapitein', 'bevelhebber']
+            'kolonel', 'kol', 'kapitein', 'bevelhebber'],
+        'schema_types': ['MilitaryPerson'],
+        'subjects': ['politics'],
+        'types': ['person']
         },
-    'athlete': {
-        'types': ['person'],
+    'sports_person': {
+        'words': ['atleet', 'sportman', 'sportvrouw', 'sporter',
+            'wielrenner', 'voetballer', 'tennisser', 'zwemmer', 'spits',
+            'keeper', 'scheidsrechter'],
         'schema_types': ['Athlete', 'SoccerPlayer', 'Cyclist', 'SoccerManager',
             'TennisPlayer', 'Swimmer', 'Boxer', 'Wrestler', 'Speedskater',
             'Skier', 'WinterSportPlayer', 'GolfPlayer', 'RacingDriver',
             'MotorsportRacer', 'Canoist', 'Cricketer', 'RugbyPlayer',
-            'HorseRider', 'AmericanFootballPlayer', 'Rower',
-            'Skater', 'BaseballPlayer', 'BasketballPlayer',
-            'SportsManager', 'IceHockeyPlayer'],
-        'words': ['atleet', 'sportman', 'sportvrouw', 'sporter',
-            'wielrenner', 'voetballer', 'tennisser', 'zwemmer'],
+            'HorseRider', 'AmericanFootballPlayer', 'Rower', 'MotorcycleRider',
+            'Skater', 'BaseballPlayer', 'BasketballPlayer', 'Gymnast',
+            'SportsManager', 'IceHockeyPlayer', 'FigureSkater', 'HandballPlayer'],
+        'subjects': ['sports'],
+        'types': ['person']
         },
     'artist': {
-        'types': ['person'],
-        'schema_types': ['MusicalArtist', 'Artist', 'Writer', 'Actor',
-            'Painter', 'Journalist', 'Architect', 'Screenwriter',
-            'VoiceActor', 'Presenter', 'Photographer',
-            'ClassicalMusicArtist', 'Poet', 'FashionDesigner',
-            'Comedian'],
         'words': ['schrijver', 'auteur', 'acteur', 'kunstenaar',
             'schilder', 'beeldhouwer', 'architect', 'musicus',
             'schrijver', 'componist', 'fotograaf', 'dichter',
             'ontwerper', 'toneelspeler', 'filmregisseur', 'regisseur',
-            'zanger', 'zangeres', 'actrice', 'trompetspeler', 'orkestleider']
+            'zanger', 'zangeres', 'actrice', 'trompetspeler', 'orkestleider'],
+        'schema_types': ['MusicalArtist', 'Artist', 'Writer', 'Actor',
+            'Painter', 'Journalist', 'Architect', 'Screenwriter',
+            'VoiceActor', 'Presenter', 'Photographer', 'Comedian',
+            'ClassicalMusicArtist', 'Poet', 'FashionDesigner'],
+        'subjects': ['culture'],
+        'types': ['person']
         },
+    'business_person': {
+        'words': ['manager', 'teamleider', 'directeur', 'bedrijfsleider', 'ondernemer'],
+        'schema_types': [],
+        'subjects': ['business'],
+        'types': ['person']
+    },
     'scientist': {
-        'types': ['person'],
-        'schema_types': ['Scientist'],
         'words': ['prof', 'professor', 'dr', 'ingenieur', 'ir',
             'natuurkundige', 'scheikundige', 'wiskundige', 'bioloog',
-            'historicus', 'onderzoeker', 'drs', 'ing']
+            'historicus', 'onderzoeker', 'drs', 'ing', 'wetenschapper'],
+        'schema_types': ['Scientist'],
+        'subjects': ['science'],
+        'types': ['person']
         },
     'religious_person': {
-        'types': ['person'],
-        'schema_types': ['ChristianBishop', 'Cardinal', 'Cleric', 'Saint', 'Pope'],
         'words': ['dominee', 'paus', 'kardinaal', 'aartsbisschop',
             'bisschop', 'monseigneur', 'mgr', 'kapelaan', 'deken',
             'abt', 'prior', 'pastoor', 'pater', 'predikant',
-            'opperrabbijn', 'rabbijn', 'imam', 'geestelijke', 'frater']
+            'opperrabbijn', 'rabbijn', 'imam', 'geestelijke', 'frater'],
+        'schema_types': ['ChristianBishop', 'Cardinal', 'Cleric', 'Saint', 'Pope'],
+        'subjects': ['religion'],
+        'types': ['person']
         },
     # Locations
     'settlement': {
-        'types': ['location'],
+        'words': ['gemeente', 'provincie', 'stad', 'dorp', 'regio', 'wijk',
+            'gebied', 'stadsdeel', 'waterschap', 'straat'],
         'schema_types': ['Settlement', 'Village', 'Municipality', 'Town',
             'AdministrativeRegion', 'City', 'HistoricPlace', 'PopulatedPlace',
-            'ProtectedArea', 'CityDistrict', 'Country', 'MountainRange'],
-        'words': ['gemeente', 'provincie', 'stad', 'dorp', 'regio', 'wijk',
-            'gebied']
+            'ProtectedArea', 'CityDistrict', 'Country', 'SubMunicipality', 'Street'
+            'District'],
+        'subjects': [],
+        'types': ['location']
         },
     'infrastructure': {
-        'types': ['location'],
+        'words': ['station', 'metrostation', 'vliegveld', 'gebouw', 'brug', 'monument'],
         'schema_types': ['Building', 'Road', 'Station', 'RailwayStation',
-            'Airport', 'HistoricBuilding', 'Bridge', 'Dam'],
-        'words': ['station', 'metrostation', 'vliegveld', 'gebouw', 'brug']
+            'Airport', 'HistoricBuilding', 'Bridge', 'Dam', 'ArchitecturalStructure',
+            'Monument', 'Castle', 'WorldHeritageSite', 'MetroStation'],
+        'subjects': [],
+        'types': ['location']
         },
     'natural_location': {
-        'types': ['location'],
+        'words': ['rivier', 'gebergte', 'meer', 'planeet', 'eiland'],
         'schema_types': ['River', 'Mountain', 'Lake', 'CelestialBody',
-            'Asteroid', 'Planet', 'Island'],
-        'words': ['rivier', 'gebergte', 'meer', 'planeet', 'eiland']
+            'Asteroid', 'Planet', 'Island', 'MountainRange', 'BodyOfWater',
+            'MountainPass'],
+        'subjects': [],
+        'types': ['location']
+        },
+    'sports_location': {
+        'words': ['stadion', 'arena'],
+        'schema_types': ['Stadium', 'Arena'],
+        'subjects': ['sports'],
+        'types': ['location']
+        },
+    'religious_location': {
+        'words': ['bisdom', 'kerk', 'kathedraal', 'tempel', 'kapel', 'heiligdom'],
+        'schema_types': ['Church', 'ReligiousBuilding', 'Diocese'],
+        'subjects': ['religion'],
+        'types': ['location', 'organisation']
         },
     # Organizations
     'company': {
-        'types': [],
-        'schema_types': ['Company'],
-        'words': []
+        'words': ['bedrijf', 'bank', 'luchtvaartmaatschappij', 'onderneming', 
+            'hotel'],
+        'schema_types': ['Company', 'Bank', 'Airline', 'Hotel'],
+        'subjects': ['business'],
+        'types': ['organisation']
         },
     'school': {
-        'types': [],
+        'words': ['basisschool', 'school', 'hogeschool', 'universiteit', 
+            'onderzoeksinstituut', 'faculteit'],
         'schema_types': ['School', 'University'],
-        'words': []
+        'subjects': ['science'],
+        'types': ['organisation', 'location']
         },
     'political_organisation': {
-        'types': ['organisation'],
-        'schema_types': ['PoliticalParty']
+        'words': ['partij'],
+        'schema_types': ['PoliticalParty', 'GovernmentAgency'],
+        'subjects': ['politics'],
+        'types': ['organisation']
         },
     'sports_organisation': {
-        'types': ['organisation'],
-        'schema_types': ['SoccerClub']
+        'words': ['club', 'voetbalclub'],
+        'schema_types': ['SoccerClub', 'RugbyClub', 'SportsTeam', 'SoccerLeague',
+            'HockeyTeam'],
+        'subjects': ['sports'],
+        'types': ['organisation']
         },
     'cultural_organisation': {
-        'types': ['organisation'],
-        'schema_types': ['Band', 'MusicGroup'],
-        'words': ['band', 'gezelschap']
+        'words': ['museum', 'band', 'rockband', 'popgroep', 'orkest'],
+        'schema_types': ['Band', 'MusicGroup', 'RecordLabel', 'Museum'],
+        'subjects': ['culture'],
+        'types': ['organisation']
         },
     'military_organisation': {
-        'types': [],
+        'words': [],
         'schema_types': ['MilitaryUnit'],
-        'words': []
+        'subjects': ['politics'],
+        'types': ['organisation']
+        },
+    'media_organisation': {
+        'words': ['krant', 'tijdschrift', 'zender', 'televisiezender',
+            'radiozender'],
+        'schema_types': ['Newspaper', 'Magazine', 'RadioStation', 'Publisher',
+            'TelevisionStation', 'AcademicJournal', 'PeriodicalLiterature'],
+        'subjects': [],
+        'types': []
         },
     # Other
     'creative_work': {
-        'types': [],
+        'words': ['film', 'album', 'plaat', 'nummer', 'single', 'boek', 'roman',
+            'novelle', 'bundel', 'dichtbundel', 'script', 'serie', 'televisieserie', 
+            'opera', 'toneelstuk', 'gedicht', 'schilderij', 'beeld'],
         'schema_types': ['CreativeWork', 'Film', 'Album', 'Single', 'Book',
-            'TelevisionShow', 'TelevisionEpisode', 'Song', 'MusicalWork'],
-        'words': []
+            'TelevisionShow', 'TelevisionEpisode', 'Song', 'MusicalWork',
+            'ArtWork', 'WrittenWork', 'Play'],
+        'subjects': ['culture'],
+        'types': []
+        },
+    'product': {
+        'words': [],
+        'schema_types': ['Product'],
+        'subjects': ['business'],
+        'types': []
         },
     'sports_event': {
-        'types': [],
-        'schema_types': ['OlympicEvent', 'SoccerTournament',
-            'TennisTournament', 'FootballMatch'],
-        'words': []
+        'words': ['wedstrijd'],
+        'schema_types': ['OlympicEvent', 'SoccerTournament', 'GrandPrix'
+            'TennisTournament', 'FootballMatch', 'CyclingRace', 'SportsEvent'],
+        'subjects': ['sports'],
+        'types': []
         },
     'military_event': {
-        'types': [],
+        'words': ['oorlog', 'conflict'],
         'schema_types': ['MilitaryConflict'],
-        'words': []
+        'subjects': ['politics'],
+        'types': []
         }
     }
 
 subjects = {
-    'politics': {
-        'roles': ['politician', 'royalty', 'military_person',
-            'political_orginisation', 'military_organisation', 'military_event'],
-        'words': ['regering', 'kabinet', 'fractie', 'partij',
-            'tweede kamer', 'eerste kamer', 'politiek', 'politicus',
-            'vorstenhuis', 'koningshuis', 'koninklijk huis', 'troon', 'rijk',
-            'keizerrijk', 'monarchie', 'leger', 'oorlog', 'troepen', 'gevecht',
-            'strijd', 'strijdkrachten']
-    'sports': {
-        'roles': ['athlete', 'sports_organisation', 'sports_event'],
-        'words': ['sport', 'voetbal', 'wielersport', 'wedstrijd']
-        },
-    'culture': {
-        'roles': ['artist', 'cultural_organisation', 'creative_work'],
-        'words': ['kunst', 'cultuur', 'roman', 'boek', 'gedicht',
-            'bundel', 'werk', 'schilderij', 'beeld', 'muziek',
-            'toneel', 'theater', 'film']
-        },
-    'science': {
-        'roles': ['scientist', 'school'],
-        'words': ['wetenschap', 'wetenschapper', 'studie',
-            'onderzoek', 'uitvinding', 'ontdekking']
-        },
-    'religion': {
-        'roles': ['religious_person'],
-        'words': ['kerk', 'parochie', 'geloof', 'religie']
-        }
+    'politics': ['regering', 'kabinet', 'fractie', 'tweede kamer',
+        'eerste kamer', 'politiek', 'vorstenhuis', 
+        'koningshuis', 'koninklijk huis', 'troon', 'rijk',
+        'keizerrijk', 'monarchie', 'leger', 'oorlog', 'troepen',
+        'strijdkrachten'],
+    'sports': ['sport', 'voetbal', 'wielersport', 'speler', 'spelers'],
+    'culture': ['kunst', 'cultuur', 'muziek', 'toneel', 'theater', 'film'],
+    'business': ['economie', 'beurs', 'aandelen', 'bedrijfsleven', 
+        'management', 'werknemer', 'werknemers', 'salaris', 'staking', 
+        'personeel'],
+    'science': ['wetenschap', 'studie', 'onderzoek', 'uitvinding', 'ontdekking'],
+    'religion': ['geloof', 'religie']
     }
-
