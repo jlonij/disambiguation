3431a3dd99c06ebd41537e4fecf212205503975a
Juliette Lonij <jlonij@gmail.com>
2017-03-19 07:10:45 +0100

    Improve string normalization

--- utilities.py
+++ utilities.py
@@ -31,7 +31,7 @@ def clean(s):
     '''
     chars = ['+', '=', '^', '*', '~', '#', '_', '\\']
     chars += ['(', ')', '[', ']', '{', '}', '<', '>']
-    chars += ['\'', '"', '`', u'„']
+    chars += ['\'', '"', '`']
     for c in chars:
         s = s.replace(c, u'')
     s = u' '.join(s.split())
@@ -41,14 +41,14 @@ def normalize(s):
     '''
     Normalize string by removing punctuation, capitalization, diacritics.
     '''
+    # Replace diactritics
+    s = unidecode(s)
     # Remove unwanted characters
     s = clean(s)
-    # Remove diactritics
-    s = unidecode(s)
     # Remove capitalization
     s = s.lower()
     # Replace regular punctuation by spaces
-    chars = ['.', ',', ':', '?', '!', ';', '-', '\u2013', '/', '|', '&']
+    chars = ['.', ',', ':', '?', '!', ';', '-', '/', '|', '&']
     for c in chars:
         s = s.replace(c, u' ')
     s = u' '.join(s.split())
