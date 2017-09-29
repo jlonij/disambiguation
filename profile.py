88110c29c2ea460423aeee4586e1a827c46b9945
Juliette Lonij <juliette.lonij@kb.nl>
2017-09-29 12:57:34 +0200

    Update profile.py

--- profile.py
+++ profile.py
@@ -3,14 +3,17 @@
 
 import dac
 
+from pycallgraph import GlobbingFilter
 from pycallgraph import PyCallGraph
 from pycallgraph import Config
 from pycallgraph.output import GraphvizOutput
 
 config = Config(max_depth=5)
-graphviz = GraphvizOutput(output_file='filter_max_depth_article.png')
+config.trace_filter = GlobbingFilter(exclude=['lxml.*', 'requests.*'])
+graphviz = GraphvizOutput(output_file='out.png')
 
 with PyCallGraph(output=graphviz, config=config):
     linker = dac.EntityLinker(model='svm', debug=True, features=True,
                 candidates=True)
     linker.link('http://resolver.kb.nl/resolve?urn=ddd:010734861:mpeg21:a0002:ocr')
+
