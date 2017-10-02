35d4be688329da340392ae7cd571fec6173f7916
Juliette Lonij <jlonij@gmail.com>
2017-10-02 22:57:12 +0200

    Update profile.py

--- profile.py
+++ profile.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import dac
+import time
 
 from pycallgraph import GlobbingFilter
 from pycallgraph import PyCallGraph
 from pycallgraph import Config
 from pycallgraph.output import GraphvizOutput
 
-config = Config(max_depth=5)
-config.trace_filter = GlobbingFilter(exclude=['lxml.*', 'requests.*', 'sklearn.*'])
-graphviz = GraphvizOutput(output_file='profile.png')
+config = Config(max_depth=5, verbose=True)
+config.trace_filter = GlobbingFilter(exclude=['lxml.*', 'requests.*',
+    'sklearn.*', 'pycallgraph.*'])
+graphviz = GraphvizOutput(output_file='profile_' + str(int(time.time())) + '.png')
 
 with PyCallGraph(output=graphviz, config=config):
     linker = dac.EntityLinker(model='svm', debug=True)
