543792f31f4c6b2845b610b85d175ff403aa6aad
Juliette Lonij <jlonij@gmail.com>
2017-10-20 20:30:23 +0200

    Update config parser

--- config.py
+++ config.py
@@ -6,8 +6,11 @@ import requests
 #CONFIG_URL = 'http://145.100.58.199:82/config.json'
 CONFIG_URL = 'http://kbresearch.nl/dac/config.json'
 
-def parse_config(config_url=CONFIG_URL):
-    response = requests.get(config_url)
-    if response.status_code == 200:
-        return json.loads(response.content)
-    return False
+def parse_config(config_url=CONFIG_URL, local=False):
+    if local:
+        return json.load(open('config.json'))
+    else:
+        response = requests.get(config_url)
+        if response.status_code == 200:
+            return json.loads(response.content)
+        return False
