0f849dbd66df0b262aff39058ffb483a8e24506a
Juliette Lonij <juliette.lonij@kb.nl>
2017-12-14 16:42:15 +0100

    Update linker parameters

--- tests/profile.py
+++ tests/profile.py
@@ -18,5 +18,6 @@ config.trace_filter = GlobbingFilter(exclude=['lxml.*', 'requests.*',
 graphviz = GraphvizOutput(output_file='profile_' + str(int(time.time())) + '.png')
 
 with PyCallGraph(output=graphviz, config=config):
-    linker = dac.EntityLinker(model='svm', debug=True, train=True)
+    linker = dac.EntityLinker(model='svm', debug=True)
     linker.link('http://resolver.kb.nl/resolve?urn=ddd:010734861:mpeg21:a0002:ocr')
+
