35b6f2eba0cd9817141c98399074795ea6c3b5d1
Juliette Lonij <juliette.lonij@kb.nl>
2018-03-26 11:56:47 +0200

    Update absolute path

--- dac/web.py
+++ dac/web.py
@@ -31,7 +31,8 @@ from bottle import response
 from bottle import route
 from bottle import run
 
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                '..'))
 from dac import dac
 
 
